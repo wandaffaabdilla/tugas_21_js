50840d83c01f9a129b30348e18056d6b5d11b01a
wan daffa abdilla <wandaffaadilla@gmail.com>
2022-03-07 14:57:48 +0800

    Tugas 21 JS
    Tugas ini kami kumpulkan untuk niomic

---
+++ tugas21.js
@@ -0,0 +1,7 @@
+function tugas21() {
+  var keinginan = "Saya belajar masak nasi goreng";
+  console.log(keinginan);
+  var result = keinginan.replace("masak", "makan");
+  return result;
+}
+console.log(tugas21());
